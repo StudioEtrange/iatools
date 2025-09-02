8275fe6dd6d6b93f9d4a252290cb20c55313aafc
StudioEtrange <nomorgan@gmail.com>
2025-09-02 23:08:23 +0200

    update stella framework, review path, add check requirements function, review launcher system, split mcp submenu

--- lib/lib_gemini.sh
+++ lib/lib_gemini.sh
@@ -1,26 +1,51 @@
 
-gemini_files() {
-    export GEMINI_CONFIG_HOME="$HOME/.gemini"
-    export GEMINI_CONFIG_CMD_HOME="$GEMINI_CONFIG_HOME/commands"
-    export GEMINI_CONFIG_FILE="$GEMINI_CONFIG_HOME/settings.json"
+gemini_path() {
+    # gc specific paths
+    export IATOOLS_GEMINI_CONFIG_HOME="${HOME}/.gemini"
+    export IATOOLS_GEMINI_CONFIG_CMD_HOME="${IATOOLS_GEMINI_CONFIG_HOME}/commands"
+    export IATOOLS_GEMINI_CONFIG_FILE="${IATOOLS_GEMINI_CONFIG_HOME}/settings.json"
+
+    # iatools path for gc
+    export IATOOLS_GEMINI_LAUNCHER_HOME="${IATOOLS_LAUNCHER_HOME}/gemini-cli"
+    mkdir -p "${IATOOLS_GEMINI_LAUNCHER_HOME}"
+
 }
 
+
+gemini_launcher_manage() {
+    if [ -f "${IATOOLS_NODEJS_BIN_PATH}gemini" ]; then
+        echo '#!/bin/sh' > "${IATOOLS_GEMINI_LAUNCHER_HOME}/gemini"
+        echo "${IATOOLS_NODEJS_BIN_PATH}node ${IATOOLS_NODEJS_BIN_PATH}/gemini \$@" >> "${IATOOLS_GEMINI_LAUNCHER_HOME}/gemini"
+        chmod +x "${IATOOLS_GEMINI_LAUNCHER_HOME}/gemini"
+
+        # launcher based on symbolic link :
+        # link doest not exist OR is not valid
+        # if [ ! -L "${IATOOLS_GEMINI_LAUNCHER_HOME}/gemini" ] || [ ! -e "${IATOOLS_GEMINI_LAUNCHER_HOME}/gemini" ]; then
+        #     echo "Create a gemini launcher"
+        #     ln -fsv "${IATOOLS_NODEJS_BIN_PATH}/gemini" "${IATOOLS_GEMINI_LAUNCHER_HOME}/gemini"
+        # fi
+    else
+        rm -f "${IATOOLS_GEMINI_LAUNCHER_HOME}/gemini"
+    fi
+}
+
+
 gemini_settings_configure() {
-    merge_json_file "${_CURRENT_FILE_DIR}/pool/settings/gemini-cli/settings.json" "$GEMINI_CONFIG_FILE"
+    merge_json_file "${_CURRENT_FILE_DIR}/pool/settings/gemini-cli/settings.json" "$IATOOLS_GEMINI_CONFIG_FILE"
 }
 
 gemini_settings_remove() {
-    rm -Rf "$GEMINI_CONFIG_HOME"
+    rm -Rf "$IATOOLS_GEMINI_CONFIG_HOME"
 }
 
 gemini_merge_config() {
     file_to_merge="$1"
-    merge_json_file "$file_to_merge" "$GEMINI_CONFIG_FILE"
+    merge_json_file "$file_to_merge" "$IATOOLS_GEMINI_CONFIG_FILE"
 }
 
 gemini_remove_config() {
     key_path="$1"
-    json_remove_key "$key_path" "$GEMINI_CONFIG_FILE"
+    json_remove_key "$key_path" "$IATOOLS_GEMINI_CONFIG_FILE"
 }
 
 gemini_add_command() {
@@ -31,16 +56,13 @@ gemini_add_command() {
         exit 1
     fi
 
-    mkdir -p "${GEMINI_CONFIG_CMD_HOME}"
+    mkdir -p "${IATOOLS_GEMINI_CONFIG_CMD_HOME}"
 
-    cp -f "${command_file}" "${GEMINI_CONFIG_CMD_HOME}/"
+    cp -f "${command_file}" "${IATOOLS_GEMINI_CONFIG_CMD_HOME}/"
 }
 
 gemini_remove_command() {
     command_file="$1"
 
-    if [ ! -f "${GEMINI_CONFIG_CMD_HOME}/${command_file}" ]; then
-        return
-    fi
-    rm -f "${GEMINI_CONFIG_CMD_HOME}/${command_file}"
+    rm -f "${IATOOLS_GEMINI_CONFIG_CMD_HOME}/${command_file}"
 }
--- lib/lib_opencode.sh
+++ lib/lib_opencode.sh
@@ -1,27 +1,46 @@
 
-opencode_files() {
-    export OPENCODE_LOCAL_SHARE_HOME="$HOME/.local/share/opencode"
-    export OPENCODE_CONFIG_HOME="$HOME/.config/opencode"
+opencode_path() {
+    # oc specific paths
+    export IATOOLS_OPENCODE_LOCAL_SHARE_HOME="$HOME/.local/share/opencode"
+    export IATOOLS_OPENCODE_CONFIG_HOME="$HOME/.config/opencode"
     #You can also specify a custom config file path using the OPENCODE_CONFIG environment variable. This takes precedence over the global and project configs.
-    [ "$OPENCODE_CONFIG" == "" ] && export OPENCODE_CONFIG_FILE="$OPENCODE_CONFIG_HOME/opencode.json" || export OPENCODE_CONFIG_FILE="$OPENCODE_CONFIG"
+    [ "$OPENCODE_CONFIG" = "" ] && export IATOOLS_OPENCODE_CONFIG_FILE="$IATOOLS_OPENCODE_CONFIG_HOME/opencode.json" || export IATOOLS_OPENCODE_CONFIG_FILE="$OPENCODE_CONFIG"
+
+    # iatools path for oc
+    export IATOOLS_OPENCODE_LAUNCHER_HOME="${IATOOLS_LAUNCHER_HOME}/opencode"
+    mkdir -p "${IATOOLS_OPENCODE_LAUNCHER_HOME}"
+
+
 }
 
+opencode_launcher_manage() {
+    if [ -f "${IATOOLS_NODEJS_BIN_PATH}opencode" ]; then
+        # launcher based on a symbolic link :
+        # link doest not exist OR is not valid
+        if [ ! -L "${IATOOLS_OPENCODE_LAUNCHER_HOME}/opencode" ] || [ ! -e "${IATOOLS_OPENCODE_LAUNCHER_HOME}/opencode" ]; then
+            echo "Create an opencode launcher"
+            ln -fsv "${IATOOLS_NODEJS_BIN_PATH}opencode" "${IATOOLS_OPENCODE_LAUNCHER_HOME}/opencode"
+        fi
+    else
+        rm -f "${IATOOLS_OPENCODE_LAUNCHER_HOME}/opencode"
+    fi
+}
 
 opencode_settings_configure() {
-    merge_json_file "${_CURRENT_FILE_DIR}/pool/settings/opencode/opencode.json" "$OPENCODE_CONFIG_FILE"
+    merge_json_file "${_CURRENT_FILE_DIR}/pool/settings/opencode/opencode.json" "$IATOOLS_OPENCODE_CONFIG_FILE"
 }
 
 opencode_settings_remove() {
-    rm -Rf "$OPENCODE_LOCAL_SHARE_HOME"
-    rm -Rf "$OPENCODE_CONFIG"
+    rm -Rf "$IATOOLS_OPENCODE_LOCAL_SHARE_HOME"
+    rm -Rf "$IATOOLS_OPENCODE_CONFIG_HOME"
 }
 
 opencode_merge_config() {
     file_to_merge="$1"
-    merge_json_file "$file_to_merge" "$OPENCODE_CONFIG_FILE"
+    merge_json_file "$file_to_merge" "$IATOOLS_OPENCODE_CONFIG_FILE"
 }
 
 opencode_remove_config() {
     key_path="$1"
-    json_remove_key "$key_path" "$OPENCODE_CONFIG_FILE"
+    json_remove_key "$key_path" "$IATOOLS_OPENCODE_CONFIG_FILE"
 }
